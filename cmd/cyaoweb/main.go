256c6ca7a050746044868bbb96aa57237da57fc7
Beadko <beadko@gmail.com>
2025-03-15 12:38:51 +0700

    Build the http.Handler for the story

--- cmd/cyaoweb/main.go
+++ cmd/cyaoweb/main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 
-	cyao "github.com/Beadko/gophercizes_cyoa"
+	cyoa "github.com/Beadko/gophercizes_cyoa"
 )
 
 func main() {
+	port := flag.Int("port", 3000, "The port to start the CYOA web application on")
 	fileName := flag.String("file", "gopher.json", "the JSON file with the CYOA stroy")
 	flag.Parse()
 	fmt.Printf("Using the story %s.\n", *fileName)
@@ -19,11 +22,13 @@ func main() {
 		return
 	}
 
-	story, err := cyao.JSONStory(f)
+	story, err := cyoa.JSONStory(f)
 	if err != nil {
 		fmt.Printf("Could not parse the struct from %s: %v\n", *fileName, err)
 		return
 	}
 
-	fmt.Printf("%+v\n", story)
+	h := cyoa.NewHandler(story)
+	fmt.Printf("Starting the server on port: %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
 }
--- story.go
+++ story.go
@@ -1,9 +1,39 @@
-package cyao
+package cyoa
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
+	"net/http"
+	"text/template"
 )
+
+func init() {
+	tmpl = template.Must(template.New("").Parse(defaultHandlerTmpl))
+}
+
+var tmpl *template.Template
+
+var defaultHandlerTmpl = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Choose Your Own Adventure</title>
+</head>
+<body>
+    <h1>{{.Title}}</h1>
+    {{range .Paragraphs}}
+    <p>{{.}}</p>
+    {{end}}
+    <ul>
+        {{range .Options}}
+            <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+        {{end}}
+    </ul>
+</body>
+</html>`
 
 type Story map[string]Chapter
 
@@ -26,4 +56,19 @@ func JSONStory(r io.Reader) (Story, error) {
 		return nil, err
 	}
 	return story, nil
+}
+
+type handler struct {
+	s Story
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if err := tmpl.Execute(w, h.s["intro"]); err != nil {
+		fmt.Printf("Error executing story %s: %v", h.s["intro"], err)
+	}
+
+}
+
+func NewHandler(s Story) http.Handler {
+	return handler{s}
 }
